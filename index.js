9ea88abcecfa509d8707a61a5278f29feadda666
groov1234 <c.groover@outlook.com>
2018-06-15 15:05:32 -0400

    some greensock goodness added

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var jumbo = 
+    document.getElementById("jumbo");
+        TweenLite.to(jumbo, 2, {boxShadow:"0px 0px 20px red", color:"#FC0"});
+    var box =
+    document.getElementsByName("box");
+    TweenLite.from(box, 2, {width:10, height:10});
+}
